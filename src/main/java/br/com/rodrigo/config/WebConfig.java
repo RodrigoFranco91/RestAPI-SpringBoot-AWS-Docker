d857bd5452af5e9637058c43ceca2a869ddd597e
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2020-12-08 16:20:37 -0300

    Habilitando CORS global da API

--- src/main/java/br/com/rodrigo/config/WebConfig.java
+++ src/main/java/br/com/rodrigo/config/WebConfig.java
@@ -6,63 +6,57 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.rodrigo.serialization.converter.YamlJackson2HttpMessageConverter;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
-	
+public class WebConfig implements WebMvcConfigurer {
+
 	private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
-	
+
+	public void addCorsMapping(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE",
+				"CONNECT");
+	}
+
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
 	}
-	
+
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		
-		//Via Extension. localhost:8080/pessoa.json
+
+		// Via Extension. localhost:8080/pessoa.json
 		/*
-		configurer.favorParameter(false)
-		.ignoreAcceptHeader(false)
-		.defaultContentType(MediaType.APPLICATION_JSON)
-		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
-		*/
-		
-		//Via Query Param. localhost:8080/pessoa?mediaType=xml
+		 * configurer.favorParameter(false) .ignoreAcceptHeader(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+		 */
+
+		// Via Query Param. localhost:8080/pessoa?mediaType=xml
 		/*
-		configurer.favorPathExtension(false)
-		.favorParameter(true)
-		.parameterName("mediaType")
-		.ignoreAcceptHeader(true)
-		.useRegisteredExtensionsOnly(false)
-		.defaultContentType(MediaType.APPLICATION_JSON)
-		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
-		*/
-		
-		//Via Header. Key = Accpet ~ Value= application/json . localhost:8080/pessoa
+		 * configurer.favorPathExtension(false) .favorParameter(true)
+		 * .parameterName("mediaType") .ignoreAcceptHeader(true)
+		 * .useRegisteredExtensionsOnly(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+		 */
+
+		// Via Header. Key = Accpet ~ Value= application/json . localhost:8080/pessoa
 
 		/*
-		configurer.favorPathExtension(false)
-		.favorParameter(false)
-		.ignoreAcceptHeader(false)
-		.useRegisteredExtensionsOnly(false)
-		.defaultContentType(MediaType.APPLICATION_JSON)
-		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
-		*/
-		
-		configurer.favorPathExtension(false)
-		.favorParameter(false)
-		.ignoreAcceptHeader(false)
-		.useRegisteredExtensionsOnly(false)
-		.defaultContentType(MediaType.APPLICATION_JSON)
-		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML)
-		.mediaType("x-yaml", MEDIA_TYPE_YML);
+		 * configurer.favorPathExtension(false) .favorParameter(false)
+		 * .ignoreAcceptHeader(false) .useRegisteredExtensionsOnly(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+		 */
+
+		configurer.favorPathExtension(false).favorParameter(false).ignoreAcceptHeader(false)
+				.useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
+				.mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML)
+				.mediaType("x-yaml", MEDIA_TYPE_YML);
 	}
 }
 
--- src/main/java/br/com/rodrigo/controller/PessoaController.java
+++ src/main/java/br/com/rodrigo/controller/PessoaController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import br.com.rodrigo.service.PessoaService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin
 @Api(value = "Pessoa End-point", description = "Descrição para pessoa", tags = {"Pessoa TAG", "Tag de Pessoa"})
 @RestController
 @RequestMapping("/pessoa")
